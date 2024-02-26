25219ed3a1d478b824f79648be7fc09eec0ae08b
Sagar-Ahir <sagar.ahir@equisinfo.in>
2024-02-26 14:52:21 +0530

    added error boundary

--- template/App.tsx
+++ template/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 import Navigation from './src/Navigation/Navigation';
-import {persistor, store} from './src/Store/Store';
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
+import { persistor, store } from './src/Store/Store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import ErrorBoundary from './src/Components/ErrorBoundary';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <Navigation />
+          <ErrorBoundary>
+            <Navigation />
+          </ErrorBoundary>
         </PersistGate>
       </Provider>
     </SafeAreaView>
